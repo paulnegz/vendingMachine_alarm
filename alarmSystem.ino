fcc50475086c78a1c4db12a55ad7cd6db83ec359
Paul Negedu <paul.negedu@yahoo.com>
2022-06-15 07:28:42 -0500

    namespaces

--- alarmSystem.ino
+++ alarmSystem.ino
@@ -1,21 +1,19 @@
-#include <Arduino.h>
-#include <PCM.h>
 #include "namespaces.h"
 using machine::isShaking;
 using namespace sound;
 using namespace sensor;
-namespace sensor::powerPin{
+
+namespace powerPin{
   constexpr uint8_t high[] {2,4};
   constexpr uint8_t low[] {3,5};
-} using sensor::powerPin::high;
-
+};
 void powerOn(){
   Serial.begin(115200);
-  for(const auto& pin: high){
+  for(const auto& pin: powerPin::high){
     pinMode(pin,OUTPUT);
     digitalWrite(pin, HIGH);
   }
-  for(const auto& pin: sensor::powerPin::low){
+  for(const auto& pin: powerPin::low){
     pinMode(pin,OUTPUT);
     digitalWrite(pin, LOW);
   }
@@ -29,17 +27,14 @@ void loop() {
   vibration = analogRead(sensor::pin::vibration); 
   sensor::printValues(tilt, vibration);
 
-  machine::shaking = isShaking(tilt, vibration, prev_vibration); 
+  machine::shaking = isShaking(tilt, vibration); 
   if(machine::shaking && !sound::playAlarm){
     startTime = millis();
-    startPlayback(alarm::recording, alarm::size);
-    playAlarm = true;
+    alarm::play();
   }
   timerUp = millis() > startTime+alarm::duration;
-  if(sound::playAlarm && timerUp){
-    stopPlayback();
-    playAlarm = false;
-  }
+  if(sound::playAlarm && timerUp)
+    alarm::stop();
 
   prev_vibration = vibration;
   prev_tilt = tilt;
